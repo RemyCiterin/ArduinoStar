cdf93eaffb988c953c2efa8e77c68fa9de96969f
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2023-04-20 18:45:13 +0200

    libc: minor changes and .ccls

--- include/base.h
+++ include/base.h
@@ -1,2 +1,11 @@
 #define uint8_t char
 #define uint16_t short
+
+inline void write_ref_u8(uint16_t ref, uint8_t value) {
+    *(uint8_t *)ref = value;
+}
+
+inline uint8_t read_ref_u8(uint16_t ref) {
+    return *(uint8_t *)ref;
+}
+
--- lib/c/mmio.c
+++ lib/c/mmio.c
@@ -1,5 +1,5 @@
 #include "../../include/base.h"
 
 extern void gpio_write(uint8_t port, uint8_t val) {
-
+    uint8_t truc = read_ref_u8(12);
 }
